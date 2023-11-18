7aad547b1f1c75e0dde4445c823aca45b8f4cdeb
dsvivass <dsvivass@unal.edu.co>
2023-11-17 21:39:56 -0500

    Controllers added

--- src/airline-airport/airline-airport.module.ts
+++ src/airline-airport/airline-airport.module.ts
@@ -3,9 +3,11 @@ import { AirlineAirportService } from './airline-airport.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AirlineEntity } from 'src/airline/airline.entity';
 import { AirportEntity } from 'src/airport/airport.entity';
+import { AirlineAirportController } from './airline-airport.controller';
 
 @Module({
   providers: [AirlineAirportService],
   imports: [TypeOrmModule.forFeature([AirlineEntity, AirportEntity])],
+  controllers: [AirlineAirportController],
 })
 export class AirlineAirportModule {}
